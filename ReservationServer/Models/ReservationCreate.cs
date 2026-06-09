f500952c702086895d7f11bb7babb73ced7825f4
shibuyayuki0 <yshibuya@youtheng.co.jp>
2026-06-09 17:20:57 +0900

    Server側のコードとSQLのコード

---
+++ ReservationServer/Models/ReservationCreate.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+namespace ReservationServer.Models;
+
+public class ReservationCreate
+{
+    [Required]
+    [StringLength(20)]
+    public string ConfrenceName { get; set; }
+    
+
+    [Required]
+    [Range(typeof(DateTime),"1753-01-01","9999-12-31")]
+    public string Date { get; set; }
+
+
+    [Required]
+    [Range(typeof(TimeOnly),"09:00","18:00")]
+    public TimeOnly StartTime { get; set; }
+
+
+    [Required]
+    [Range(typeof(TimeOnly), "09:00", "18:00")]
+    public TimeOnly EndTime { get; set; }
+
+
+    [Required]
+    [StringLength (20)]
+    public string ReservationName { get; set; }
+}
---
+++ ReservationServer/Repositries/IReservationRepositry.cs
@@ -0,0 +1,13 @@
+﻿using ReservationServer.Models;
+
+namespace ReservationServer.Repositries;
+
+public interface IReservationRepositry
+{
+    Task<List<Reservation>> GetShowAsync(DateTime startDate);
+
+    Task PostInsert(Reservation reservation);
+
+    Task DeleteAsync(int id);
+
+}
