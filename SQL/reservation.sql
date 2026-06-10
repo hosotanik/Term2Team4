8079da0cd78e2bc33ed63fbc8bc964ffcf00ee76
shibuyayuki0 <yshibuya@youtheng.co.jp>
2026-06-10 09:23:04 +0900

    SQLのコード修正

--- SQL/reservation.sql
+++ SQL/reservation.sql
@@ -10,6 +10,8 @@ CREATE TABLE reservation
 );
 
 INSERT INTO reservation(conference_name,start_date,end_date,reservation_name)
-VALUES(N'会議室A','2026-06-09 10:00:00','2026-06-15 11:00:00',N'細谷')
+VALUES(N'会議室A','2026-06-09 10:00:00','2026-06-09 11:00:00',N'細谷')
 
 SELECT * FROM reservation
+
+drop table reservation;
